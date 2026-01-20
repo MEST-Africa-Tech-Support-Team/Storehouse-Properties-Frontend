2fd50cf1b5c29bd6f41d67599cd4eba1e77c7b51
Seth Megbetor <sethmegbetor@gmail.com>
2026-01-20 11:14:06 +0000

    Simplify main render by removing AuthProvider
    Removed AuthProvider wrapper from the main render.

--- src/main.jsx
+++ src/main.jsx
@@ -10,17 +10,28 @@
 // );
 
 
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+// import { RouterProvider } from "react-router-dom";
+// import { AuthProvider } from "./context/AuthContext";
+// import App from "./App.jsx";
+// import "./index.css";
+
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <AuthProvider>
+//       <App />
+//     </AuthProvider>
+//   </StrictMode>
+// );
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
 import App from "./App.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <App />
   </StrictMode>
 );
