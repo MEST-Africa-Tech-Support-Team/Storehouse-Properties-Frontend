da9efb2f9d6d15e946487f884055d253a3c91497
Seth Megbetor <sethmegbetor@gmail.com>
2026-01-20 14:13:01 +0000

    from seth

---
+++ src/pages/adminDashboard/properties.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AdminPropertiesPage = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Admin Properties2</h1>
+    </div>
+  );
+};
+
+export default AdminPropertiesPage;
