01e456536fba7b24e58231682a57721dbc9ba74a
Seth Megbetor <sethmegbetor@gmail.com>
2026-01-20 10:42:23 +0000

    Refactor main.jsx to include AuthProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
+// import { StrictMode } from 'react';
+// import { createRoot } from 'react-dom/client';
+// import App from './App.jsx';
+// import './index.css';
 
-createRoot(document.getElementById('root')).render(
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
+
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import App from "./App.jsx";
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </StrictMode>
 );
